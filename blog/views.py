c95744b792f81e3e8615fb52c14c17685987e65c
EsperanzaA <esperanzaa@Macs-MacBook-Pro-99.local>
2017-07-12 10:58:07 -0700

    YAYAYA

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})from django.shortcuts import render
+    return render(request, 'blog/post_list.html', {})
 
 # Create your views here.
